5433e078435e4b411478dd44b163b84dafb93804
Kaan <kaanasutay10@gmail.com>
2024-02-26 23:43:43 +0300

    controller basic

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -2,3 +2,28 @@
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
+// User Controller:
+
+module.exports = {
+
+    list: async (req, res) => {
+
+    },
+
+    create: async (req, res) => {
+
+    },
+
+    read: async (req, res) => {
+
+    },
+
+    update: async (req, res) => {
+
+    },
+
+    delete: async (req, res) => {
+
+    }
+
+}
