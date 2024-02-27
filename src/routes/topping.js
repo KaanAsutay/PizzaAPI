48f77a58e05bf68a52e177035f871d89621cb4eb
Kaan <kaanasutay10@gmail.com>
2024-02-28 00:07:06 +0300

    topping routes

--- src/routes/topping.js
+++ src/routes/topping.js
@@ -4,6 +4,21 @@
 ------------------------------------------------------- */
 const router = require('express').Router()
 /* ------------------------------------------------------- */
+// routes/topping:
+
+const topping = require('../controllers/topping')
+
+// URL: /toppings
+
+router.route('/')
+    .get(topping.list)
+    .post(topping.create)
+
+router.route('/:id')
+    .get(topping.read)
+    .put(topping.update)
+    .patch(topping.update)
+    .delete(topping.delete)
 
 /* ------------------------------------------------------- */
 module.exports = router
