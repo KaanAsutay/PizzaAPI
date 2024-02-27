10fbe9ffc3dddf20b5c6b23a091c5bc518a2f6ab
Kaan <kaanasutay10@gmail.com>
2024-02-27 22:49:15 +0300

    add swagger

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -7,23 +7,46 @@
 module.exports = {
 
     list: async (req, res) => {
-
+        /*
+            #swagger.tags = ["Users"]
+            #swagger.summary = "List Users"
+            #swagger.description = `
+                You can send query with endpoint for search[], sort[], page and limit.
+                <ul> Examples:
+                    <li>URL/?<b>search[field1]=value1&search[field2]=value2</b></li>
+                    <li>URL/?<b>sort[field1]=1&sort[field2]=-1</b></li>
+                    <li>URL/?<b>page=2&limit=1</b></li>
+                </ul>
+            `
+        */
     },
 
     create: async (req, res) => {
-
+         /*
+            #swagger.tags = ["Users"]
+            #swagger.summary = "Create User"
+        */
     },
 
     read: async (req, res) => {
-
+        /*
+            #swagger.tags = ["Users"]
+            #swagger.summary = "Get Single User"
+        */
     },
 
     update: async (req, res) => {
-
+        /*
+            #swagger.tags = ["Users"]
+            #swagger.summary = "Update User"
+        */
     },
 
     delete: async (req, res) => {
-
+        /*
+            #swagger.tags = ["Users"]
+            #swagger.summary = "Delete User"
+        */
     }
 
 }
