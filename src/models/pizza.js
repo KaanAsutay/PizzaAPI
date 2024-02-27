1fac316237bcada074af05ba04387abfbbd18f16
Kaan <kaanasutay10@gmail.com>
2024-02-28 00:12:07 +0300

    pizza model

--- src/models/pizza.js
+++ src/models/pizza.js
@@ -4,3 +4,38 @@
 ------------------------------------------------------- */
 const { mongoose } = require('../configs/dbConnection')
 /* ------------------------------------------------------- */
+// Pizza Model:
+
+const passwordEncrypt = require('../helpers/passwordEncrypt')
+
+const PizzaSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        trim: true,
+        reqired: true,
+        unique: true
+    },
+
+    image: {
+        type: String,
+        trim: true
+    },
+
+    price: {
+        type: Number,
+        reqired: true,
+    },
+
+    toppings: [ // push, pull
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Topping'
+        }
+    ]
+
+
+}, {collection: 'pizzas', timestamps: true})
+
+/* ------------------------------------------------------- */
+module.exports = mongoose.model('Pizza', PizzaSchema)
