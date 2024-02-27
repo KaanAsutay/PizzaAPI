8841a13e48d6864352711379744ce84a942217ff
Kaan <kaanasutay10@gmail.com>
2024-02-28 00:19:39 +0300

    order model

--- src/models/order.js
+++ src/models/order.js
@@ -4,3 +4,46 @@
 ------------------------------------------------------- */
 const { mongoose } = require('../configs/dbConnection')
 /* ------------------------------------------------------- */
+// Order Model:
+
+const passwordEncrypt = require('../helpers/passwordEncrypt')
+
+const OrderSchema = new mongoose.Schema({
+
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        reqired: true,
+    },
+
+    pizzaId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Pizza',
+        reqired: true,
+    },
+
+    size: {
+        type: String,
+        trim: true,
+        reqired: true,
+        enum: ['Small', 'Medium', 'Large', 'XLarge']
+    },
+
+    quantity: {
+        type: Number,
+        reqired: true,
+    },
+
+    price: {
+        type: Number,
+        reqired: true,
+    },
+
+    totalPrice: {
+        type: Number
+    }
+
+}, {collection: 'orders', timestamps: true})
+
+/* ------------------------------------------------------- */
+module.exports = mongoose.model('Order', OrderSchema)
