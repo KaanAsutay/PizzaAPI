6c67e56c3ff135f428691e9fde606767df4159df
Kaan <kaanasutay10@gmail.com>
2024-02-28 20:21:35 +0300

    auth routes

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,6 +4,15 @@
 ------------------------------------------------------- */
 const router = require('express').Router()
 /* ------------------------------------------------------- */
+// routes/auth:
+
+const auth = require('../controllers/auth')
+
+// URL: /auth
+
+router.post('/login', auth.login)
+router.post('/refresh', auth.refresh)
+router.get('/logout', auth.logout)
 
 /* ------------------------------------------------------- */
 module.exports = router
