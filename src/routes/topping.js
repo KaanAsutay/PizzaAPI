201d257d632c769ea5e792d6a02ea36cb39bdab0
Kaan <kaanasutay10@gmail.com>
2024-03-01 21:53:34 +0300

    add permissions

--- src/routes/topping.js
+++ src/routes/topping.js
@@ -6,9 +6,22 @@ const router = require('express').Router()
 /* ------------------------------------------------------- */
 // routes/topping:
 
+const permissions = require('../middlewares/permissions')
 const topping = require('../controllers/topping')
 
 // URL: /toppings
+
+// router.route('/')
+//     .get(permissions.isAdmin, topping.list)
+//     .post(permissions.isAdmin, topping.create)
+
+// router.route('/:id')
+//     .get(permissions.isAdmin, topping.read)
+//     .put(permissions.isAdmin, topping.update)
+//     .patch(permissions.isAdmin, topping.update)
+//     .delete(permissions.isAdmin, topping.delete)
+
+router.use(permissions.isAdmin)
 
 router.route('/')
     .get(topping.list)
