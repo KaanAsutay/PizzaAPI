2b1ec14dd0efa2d2e90ee52a2d85a8d76a97904e
Kaan <kaanasutay10@gmail.com>
2024-03-01 21:51:43 +0300

    add permissions

--- src/routes/user.js
+++ src/routes/user.js
@@ -6,19 +6,20 @@ const router = require('express').Router()
 /* ------------------------------------------------------- */
 // routes/user:
 
+const permissions = require('../middlewares/permissions')
 const user = require('../controllers/user')
 
 // URL: /users
 
 router.route('/')
-    .get(user.list)
+    .get(permissions.isAdmin, user.list)
     .post(user.create)
 
 router.route('/:id')
-    .get(user.read)
-    .put(user.update)
-    .patch(user.update)
-    .delete(user.delete)
+    .get(permissions.isLogin, user.read)
+    .put(permissions.isLogin, user.update)
+    .patch(permissions.isLogin, user.update)
+    .delete(permissions.isAdmin, user.delete)
 
 /* ------------------------------------------------------- */
 module.exports = router
