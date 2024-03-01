64fe133e44e0571509f15d13582157dae9d69b96
Kaan <kaanasutay10@gmail.com>
2024-03-01 22:02:48 +0300

    add push and pull Toppings

--- src/controllers/pizza.js
+++ src/controllers/pizza.js
@@ -87,5 +87,51 @@ module.exports = {
             error: !data.deletedCount,
             data
         })
-    }
+    },
+
+    // Add toppings to Pizza.toppings:
+    pushToppings: async (req, res) => {
+        /*
+            #swagger.tags = ["Pizzas"]
+            #swagger.summary = "Add Toppings to Pizza"
+        */
+
+        const toppings = req.body?.toppings // ObjectId or [ ObjectIds ]
+
+        // const data = await Pizza.findOne({ _id: req.params.id })
+        // data.toppings.push(toppings)
+        // await data.save()
+        const data = await Pizza.updateOne({ _id: req.params.id }, { $push: { toppings: toppings } })
+        const newData = await Pizza.findOne({ _id: req.params.id }).populate('toppings')
+
+        res.status(202).send({
+            error: false,
+            data,
+            toppingsCount: newData.toppings.length,
+            new: newData
+        })
+    },
+
+    // Remove toppings from Pizza.toppings:
+    pullToppings: async (req, res) => {
+        /*
+            #swagger.tags = ["Pizzas"]
+            #swagger.summary = "Remove Toppings from Pizza"
+        */
+
+        const toppings = req.body?.toppings // ObjectId
+
+        // const data = await Pizza.findOne({ _id: req.params.id })
+        // data.toppings.pull(toppings)
+        // await data.save()
+        const data = await Pizza.updateOne({ _id: req.params.id }, { $pull: { toppings: toppings } })
+        const newData = await Pizza.findOne({ _id: req.params.id }).populate('toppings')
+
+        res.status(202).send({
+            error: false,
+            data,
+            toppingsCount: newData.toppings.length,
+            new: newData
+        })
+    },
 }
